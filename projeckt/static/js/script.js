1e14aa9cc092e5f0800a4a7efbd8bcdc69e1072d
Alehandro77 <shurapchta@gmail.com>
2025-03-12 19:05:00 +0300

    Update script.js

--- projeckt/static/js/script.js
+++ projeckt/static/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() { // Ensure DOM is full
 
             // Собираем данные из формы
             const formData = new FormData(this);
-
+            
             // Отправляем данные на сервер с помощью Fetch API
             fetch(profileUrl, { // Используем полученный URL
                 method: 'POST',
@@ -29,87 +29,87 @@ document.addEventListener('DOMContentLoaded', function() { // Ensure DOM is full
                 alert('Произошла ошибка при обновлении рекомендаций.');
             });
         });
+
+
+    
+        const timeSelectors = document.querySelectorAll('.time-selector');
+
+        timeSelectors.forEach(selector => {
+            selector.addEventListener('click', function(event) {
+                const target = event.target;
+
+                // Проверяем, является ли target элементом .time-value
+                if (target.classList.contains('time-value')) {
+                    const action = target.dataset.action;
+                    const targetInput = target.dataset.target;
+                    const inputElement = document.getElementById(targetInput);
+                    const displayElement = document.getElementById(targetInput + '_display');
+
+                    if (inputElement && displayElement) {
+                        let currentValue = parseInt(inputElement.value);
+
+                        // Проверяем, является ли currentValue NaN
+                        if (isNaN(currentValue)) {
+                            currentValue = 0; // Устанавливаем значение по умолчанию
+                        }
+
+                        let maxValue = targetInput.includes('hours') ? 23 : 59;
+                        let minValue = 0;
+
+                        if (action === 'increment') {
+                            currentValue = (currentValue + 1) > maxValue ? minValue : (currentValue + 1);
+                        } else if (action === 'decrement') {
+                            currentValue = (currentValue - 1) < minValue ? maxValue : (currentValue - 1);
+                        }
+
+                        inputElement.value = currentValue;
+                        displayElement.textContent = String(currentValue).padStart(2, '0');
+                    }
+                }
+            });
+        });
+
+        // Добавляем обработчики событий для валидации в реальном времени
+        const ageInput = document.getElementById('age');
+        const weightInput = document.getElementById('weight');
+        const heightInput = document.getElementById('height');
+
+        if (ageInput) {
+            ageInput.addEventListener('input', function() {
+                validateNumber(this, 'age-error', 'Возраст должен быть числом.');
+            });
+        }
+
+        if (weightInput) {
+            weightInput.addEventListener('input', function() {
+                validateNumber(this, 'weight-error', 'Вес должен быть числом.');
+            });
+        }
+
+        if (heightInput) {
+            heightInput.addEventListener('input', function() {
+                validateNumber(this, 'height-error', 'Рост должен быть числом.');
+            });
+        }
+
+        // Функция для валидации числовых полей
+        function validateNumber(input, errorId, errorMessage) {
+        const value = input.value;
+        const errorSpan = document.getElementById(errorId);
+
+        if (isNaN(value)) {
+            errorSpan.textContent = errorMessage;
+        } else {
+            const numberValue = parseFloat(value);
+            if (numberValue < 0) {
+                errorSpan.textContent = "Значение должно быть положительным.";
+            } else {
+                errorSpan.textContent = '';
+            }
+        }
+    }
     } else {
         console.warn("Форма профиля не найдена на странице.");
     }
-
-    
-    const timeSelectors = document.querySelectorAll('.time-selector');
-
-    timeSelectors.forEach(selector => {
-        selector.addEventListener('click', function(event) {
-            const target = event.target;
-
-            // Проверяем, является ли target элементом .time-value
-            if (target.classList.contains('time-value')) {
-                const action = target.dataset.action;
-                const targetInput = target.dataset.target;
-                const inputElement = document.getElementById(targetInput);
-                const displayElement = document.getElementById(targetInput + '_display');
-
-                if (inputElement && displayElement) {
-                    let currentValue = parseInt(inputElement.value);
-
-                    // Проверяем, является ли currentValue NaN
-                    if (isNaN(currentValue)) {
-                        currentValue = 0; // Устанавливаем значение по умолчанию
-                    }
-
-                    let maxValue = targetInput.includes('hours') ? 23 : 59;
-                    let minValue = 0;
-
-                    if (action === 'increment') {
-                        currentValue = (currentValue + 1) > maxValue ? minValue : (currentValue + 1);
-                    } else if (action === 'decrement') {
-                        currentValue = (currentValue - 1) < minValue ? maxValue : (currentValue - 1);
-                    }
-
-                    inputElement.value = currentValue;
-                    displayElement.textContent = String(currentValue).padStart(2, '0');
-                }
-            }
-        });
-    });
-
-    // Добавляем обработчики событий для валидации в реальном времени
-    const ageInput = document.getElementById('age');
-    const weightInput = document.getElementById('weight');
-    const heightInput = document.getElementById('height');
-
-    if (ageInput) {
-        ageInput.addEventListener('input', function() {
-            validateNumber(this, 'age-error', 'Возраст должен быть числом.');
-        });
-    }
-
-    if (weightInput) {
-        weightInput.addEventListener('input', function() {
-            validateNumber(this, 'weight-error', 'Вес должен быть числом.');
-        });
-    }
-
-    if (heightInput) {
-        heightInput.addEventListener('input', function() {
-            validateNumber(this, 'height-error', 'Рост должен быть числом.');
-        });
-    }
-
-    // Функция для валидации числовых полей
-    function validateNumber(input, errorId, errorMessage) {
-    const value = input.value;
-    const errorSpan = document.getElementById(errorId);
-
-    if (isNaN(value)) {
-        errorSpan.textContent = errorMessage;
-    } else {
-        const numberValue = parseFloat(value);
-        if (numberValue < 0) {
-            errorSpan.textContent = "Значение должно быть положительным.";
-        } else {
-            errorSpan.textContent = '';
-        }
-    }
-}
-
 
 });
